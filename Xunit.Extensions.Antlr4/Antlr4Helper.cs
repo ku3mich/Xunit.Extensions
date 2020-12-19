2f0a8045516cd6770d3eecd3da6c817c4fd24973
Serhii Kuzmychov <ku3mich@gmail.com>
2020-12-19 18:15:59 +0200

    missed

---
+++ Xunit.Extensions.Antlr4/Antlr4Helper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Microsoft.Extensions.Logging;
+
+namespace Xunit.Extensions.Antlr4
+{
+    public class Antlr4Helper<TLexer, TParser>
+        where TParser : Parser
+        where TLexer : Lexer
+    {
+        private readonly ILogger Logger;
+        private readonly IEnumerable<Func<IParseTreeListener>> ListnersFactory;
+        private readonly Func<IAntlrErrorListener<IToken>> ErrorListnerFactory;
+
+        public Antlr4Helper(ILogger logger) : this(
+            logger,
+            new Func<IParseTreeListener>[] { () => new DebugListener(logger) }, null)
+        {
+        }
+
+        public Antlr4Helper(ILogger logger,
+            IEnumerable<Func<IParseTreeListener>> listnersFactory,
+            Func<IAntlrErrorListener<IToken>> errorListnerFactory)
+        {
+            Logger = logger;
+            ListnersFactory = listnersFactory;
+            ErrorListnerFactory = errorListnerFactory;
+        }
+
+        public ITokenStream CreateStream(string text)
+        {
+            Logger.LogDebug(text);
+            var lexer = CreateLexer(text);
+            var tokenStream = new CommonTokenStream(lexer);
+            tokenStream.Fill();
+
+            Logger.LogDebug(
+                string.Join(" -> ",
+                    tokenStream.GetTokens()
+                    .Select(s => new
+                    {
+                        s.Channel,
+                        Token = lexer.Vocabulary.GetDisplayName(s.Type)
+                    })
+                    .Select(s => $"[{s.Token}:{s.Channel}]")));
+
+            return tokenStream;
+        }
+
+        public TParser CreateParser(string s) =>
+            CreateParser(CreateStream(s));
+
+        public TParser CreateParser(ITokenStream s)
+        {
+            TParser parser = (TParser)Activator.CreateInstance(typeof(TParser), s);
+
+            if (ListnersFactory != null)
+            {
+                foreach (var listnerFactory in ListnersFactory)
+                    parser.AddParseListener(listnerFactory());
+            }
+
+            if (ErrorListnerFactory != null)
+                parser.AddErrorListener(ErrorListnerFactory());
+
+            return parser;
+        }
+
+        public static TLexer CreateLexer(string input) =>
+            (TLexer)Activator.CreateInstance(typeof(TLexer), new AntlrInputStream(input));
+    }
+}
---
+++ Xunit.Extensions.Antlr4/DebugListener.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (C) 2020 Serhii Kuzmychov (ku3mich@gmail.com)
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using Microsoft.Extensions.Logging;
+
+namespace Xunit.Extensions.Antlr4
+{
+    public class DebugListener : IParseTreeListener
+    {
+        private readonly ILogger Logger;
+
+        public DebugListener(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        int Indent = 0;
+
+        protected void Output(string s, int indent)
+        {
+            Logger.LogDebug($"{new string(' ', indent)}{s}");
+        }
+
+        protected void IndentOuput(string s)
+        {
+            Output(s, Indent);
+            Indent += 2;
+        }
+        protected void UnindentOuput(string s)
+        {
+            Indent -= 2;
+            Output(s, Indent);
+        }
+
+        public void EnterEveryRule([NotNull] ParserRuleContext ctx)
+        {
+            IndentOuput($"{{ {ctx.GetType().Name}: @{ctx.Start?.Line}:{ctx.Start?.Column} ");
+        }
+
+        public void ExitEveryRule([NotNull] ParserRuleContext ctx)
+        {
+            UnindentOuput($"}} {ctx.GetType().Name} @{ctx.Stop?.Line}:{ctx.Stop?.Column} [{ctx.GetText()}]");
+        }
+
+        public void VisitErrorNode([NotNull] IErrorNode node)
+        {
+            /*
+            var e = new System.Exception("* error");
+            e.Data["node"] = node;
+            // throw e;
+            */
+        }
+
+        public void VisitTerminal([NotNull] ITerminalNode node)
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
